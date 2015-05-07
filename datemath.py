4b7548057ff1bcc88d52f230d959b6dc161de194
Luke Forehand <lforehand@localadmins-MacBook-Pro.local>
2015-05-07 12:31:22 -0500

    more datemath

--- datemath.py
+++ datemath.py
@@ -4,11 +4,13 @@
 # this script demonstrates correct transform of a datetime object
 # to a timestamp or a string
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import calendar
 now = datetime.utcnow()
 now_timestamp = calendar.timegm(now.utctimetuple())
 now_string = now.strftime('%Y-%m-%dT%H:%M:%SZ')
+yesterday = now - timedelta(days=1)
 print now
 print now_timestamp
 print now_string
+print yesterday
