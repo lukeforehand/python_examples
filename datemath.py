c4acaba0fcf81f2f26c814c832a95524e022cc78
Luke Forehand <lforehand@localadmins-MacBook-Pro.local>
2015-05-07 10:21:47 -0500

    date math script

---
+++ datemath.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+# date math seems to really suck
+# this script demonstrates correct transform of a datetime object
+# to a timestamp or a string
+
+from datetime import datetime
+import calendar
+now = datetime.utcnow()
+now_timestamp = calendar.timegm(now.utctimetuple())
+now_string = now.strftime('%Y-%m-%dT%H:%M:%SZ')
+print now
+print now_timestamp
+print now_string
